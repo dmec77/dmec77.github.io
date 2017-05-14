6f596828b056678bb3c2a0d42947671f0f0fe74b
Diane Carlton <dimargcarl@gmail.com>
2017-05-14 17:01:58 -0400

    project commit

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ $(document).ready(function() {
 
 	//console.log("JS id WORKING!");
 
+
+	var fullboard = [];
+
 	var inPlay = [];
 
-	var notAccessed = [];
+	//var notAccessed = [];
 
 	var colorList = ["DeepPink", "DarkOliveGreen", "yellow", "Coral", "PeachPuff", "MidnightBlue"];
 
@@ -14,8 +17,19 @@ $(document).ready(function() {
 	var tableCells = $(".cell");
 		//console.log(tableCells);
 
+	var startCell = $(".start-cell");
+
+	//var coordinates = int [][];
+		//console.log(startCell);	
 
 
+// $('.cell').click(function() {   
+//    var $this = $(this);
+//    var col   = $this.index();
+//    var row   = $this.closest('tr').index();
+//    console.log( [col,row] );
+// });		
+	
 	$(".start-game").on("click", function() {	
 
 		for (var i = 0; i < tableCells.length; i++) {
@@ -23,32 +37,81 @@ $(document).ready(function() {
 			var randomColor = colorList[Math.floor(Math.random() * colorList.length)];
 
 			$(tableCells[i]).css("background-color", randomColor);	
+			$(fullboard).append(tableCells);
+			$(inPlay).append(startCell);
 
-		};
-    });	
+ 		};
+ 	});	
+//     });	
 
+//       var offset = $(this).offset();
+//   event.stopPropagation();
+//   result = offset;
+//   inPlay.push(offset);
+// 	console.log(inPlay);
 
+// });
 
+// 	$(".cell").on("click", function() {
 
+// 		//if
+// 	// 	alert("ALL ARE NOW CLICKABLE");
+// 	// });
 
+// 	$(".cell").click(function( event ) {
+// 		$(inPlay).append($(this));
 
+// 		if($(".cell").css("background-color") === $(this)) && 
+// 			$.each(inPlay, function (key, value) === offset;
+// 				Alert("we hve a match");
 
 
 
 
 
+	// var clickedColor = $(this).css("background-color");
+	// $(".start-cell").css("background-color");
+	// $(this).addClass("start-cell");
+	
+//});
 
+	// $(document).on("click", function(event) {
+ //    var x = event.offset(X);
+	// 	console.log("The X-Coord is " + X +" AND The Y-Coord is " + X);
 
+	// });
+				//use a $(this) so it refers to the one clicked at that time
 
+				// use the toArray() when putting into the array maybe
 
+// $( ".cell").on( "click", function( event ) {
+//   	$(this).offset("pageX: " + event.pageX + ", pageY: " + event.pageY);
 
+// });
 
 
 
+// $( ".cell").click(function( event ) {
+//   var offset = $( this ).offset();
+//   event.stopPropagation();
+//   $( ".cell" ).text( this.tagName +
+//     " coords ( " + offset.left + ", " + offset.top + " )" );
+// });
 
+// $( ".cell").click(function( event ) {
+//   var offset = $( this ).offset();
+//   event.stopPropagation();
+// 	console.log( offset.left + ", " + offset.top  );
+// });
 
 
 
+// $(".cell").click(function( event ) {
+// 	var clickedColor = $(this).css("background-color");
+// 	$(".start-cell").css("background-color");
+// 	$(this).addClass("start-cell");
+	
+// });
 
 
 
@@ -68,6 +131,7 @@ $(document).ready(function() {
 
 
 
+	//});
 
 
 
@@ -145,7 +209,47 @@ $(document).ready(function() {
 
 
 
-});	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//});	
 
 
 
